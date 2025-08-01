0ff1e0376a55e6976a60af205dc873ae5fade538
Mason Keresty <89534948+mkeresty@users.noreply.github.com>
2025-07-31 23:24:28 -0400

    comfig

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/your-repo-name',
+  images: { unoptimized: true },
+};
+
+module.exports = nextConfig;
